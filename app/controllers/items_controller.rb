6edb2f87430d30a5a4a2d4655aebb9d0cfa0e41a
Cory Forsyth <cory.forsyth@gmail.com>
2010-12-21 15:36:52 -0500

    able to edit and delete items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,27 @@ class ItemsController < ApplicationController
   def index
     redirect_to map_url(@map)
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    @item.attributes = params[:item]
+    if @item.save
+      redirect_to edit_map_item_url(@map, @item), :notice => "Edited item."
+    else
+      render :action => "edit", :notice => "There was an issue editing this item."
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    
+    redirect_to map_url(@map), :notice => "Deleted the item."
+  end
 
   def create
     @item = Item.new(params[:item].merge({:map => @map}))
