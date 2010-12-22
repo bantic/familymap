6d7b2b48035e1ec5aa4ad5eaccbe7dd6fbeb7fc9
Cory Forsyth <cory.forsyth@gmail.com>
2010-12-21 19:49:54 -0500

    order map items

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,4 @@
 class Map < ActiveRecord::Base
   validates_uniqueness_of :title
-  has_many :items, :dependent => :destroy
+  has_many :items, :dependent => :destroy, :order => "created_at asc"
 end
